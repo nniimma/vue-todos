4d4cd24c2ad3b1c9a00610cb76335e8d4243d300
Nima <alireza.khazforoosh@adventistas.org>
2024-05-07 10:20:19 -0300

    reactive v-model with reactive method

--- src/components/TodoCreater.vue
+++ src/components/TodoCreater.vue
@@ -4,12 +4,26 @@
         <button>Create</button>
     </div>
     {{ todo }}
+
+    <div class="input-wrap">
+        <input type="text" v-model="todoState.todo">
+        <button>Create</button>
+    </div>
+    {{ todoState.todo }}
 </template>
 
 <script setup>
-import { ref } from "vue";
+    import { reactive, ref } from "vue";
 
+    // the type of data in ref method can be string
+    // to get the value in script we need to use value: console.log(todo.value)
+    // in template . value will be added automatically
     const todo = ref('test')
+
+    // in reactive method the value should be object it can't be primative value like string
+    const todoState = reactive({
+        todo: 'testing'
+    })
 </script>
 
 <style lang="scss" scoped>
