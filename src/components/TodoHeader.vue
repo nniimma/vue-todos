58b36210a9b60f888b43d1ec5404520d3fdd5a03
Nima <alireza.khazforoosh@adventistas.org>
2024-05-08 10:42:22 -0300

    seprating css files

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -17,39 +17,6 @@
     import { RouterLink } from 'vue-router';
 </script>
 
-<style lang="scss">
-    header {
-  background-color: #f1f1f1;
-  nav {
-    display: flex;
-    align-items: center;
-    padding: 25px 16px;
-
-    .branding {
-      display: flex;
-      align-items: center;
-      gap: 8px;
-      img {
-        max-width: 50px;
-      }
-
-      h1 {
-        font-size: 24px;
-      }
-    }
-
-    .nav-routes {
-      display: flex;
-      flex: 1;
-      justify-content: flex-end;
-      gap: 12px;
-      list-style: none;
-
-      a {
-        text-decoration: none;
-        color: inherit;
-      }
-    }
-  }
-}
+<style lang="scss" scoped>
+  @import '../assets/css/todo-header.css';
 </style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -23,17 +23,5 @@
 </template>
 
 <style lang="scss" scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  max-width: 500px;
-  width: 100%;
-  margin: 0 auto;
-  padding: 40px 16px;
-
-  h1 {
-    margin-bottom: 16px;
-    text-align: center;
-  }
-}
+  @import '../assets/css/todo-view.css';
 </style>
